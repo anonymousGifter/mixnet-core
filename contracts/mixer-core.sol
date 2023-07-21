25254fd62dd3361d260030c449de9fae16ec68a6
remi <remigai@bu.edu>
2023-07-21 18:20:10 +0200

    functional base

--- contracts/mixer-core.sol
+++ contracts/mixer-core.sol
@@ -6,21 +6,33 @@ import "fhevm/abstracts/EIP712WithModifier.sol";
 import "fhevm/lib/TFHE.sol";
 
 contract MixerCore {
-    struct Recipients {
+    struct Recipient {
         euint32 encryptedRecipient;
         euint32 encryptedAmount;
     }
 
     Recipient[] public pool;
+    ebool public test;
+    euint32 public test2;
+
 
     // Deposits an encrypted amount of ERC-20 with an encrypted recipient.
     function deposit(bytes calldata encryptedRecipient, bytes calldata encryptedAmount) public {
-        pool.push(Recipients(TFHE.asEuint32(encryptedRecipient), TFHE.asEuint32(encryptedAmount)));
+        pool.push(Recipient(TFHE.asEuint32(encryptedRecipient), TFHE.asEuint32(encryptedAmount)));
     }
 
     // Withdraws an encrypted amount of ERC-20.
     function withdraw(bytes calldata encryptedAmount) public {
+        euint32 encryptedAddress = TFHE.asEuint32(addressTo32Bits(msg.sender));
+        test2 = encryptedAddress;
 
+        uint32 sum = 0;
+        for (uint32 i = 0; i < pool.length; i++) {
+            sum += i;
+            ebool b = TFHE.eq(encryptedAddress, pool[i].encryptedRecipient);
+            TFHE.req(b);
+            test = b;
+        }
     }
 
     // Function to convert an Ethereum address to a 32-bit representation
