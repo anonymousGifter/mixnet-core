1e43682beb72a7ee73be51bf3cff39a5f67eb92d
remi <remigai@bu.edu>
2023-07-21 17:40:10 +0200

    test

--- contracts/mixer-core.sol
+++ contracts/mixer-core.sol
@@ -6,14 +6,16 @@ import "fhevm/abstracts/EIP712WithModifier.sol";
 import "fhevm/lib/TFHE.sol";
 
 contract MixerCore {
-    
     struct Recipients {
-        bytes encryptedRecipient;
-        bytes encryptedAmount;
+        euint32 encryptedRecipient;
+        euint32 encryptedAmount;
     }
+
+    Recipient[] public pool;
 
     // Deposits an encrypted amount of ERC-20 with an encrypted recipient.
     function deposit(bytes calldata encryptedRecipient, bytes calldata encryptedAmount) public {
+        pool.push(Recipients(TFHE.asEuint32(encryptedRecipient), TFHE.asEuint32(encryptedAmount)));
     }
 
     // Withdraws an encrypted amount of ERC-20.
